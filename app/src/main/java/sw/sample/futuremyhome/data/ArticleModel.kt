dd71549563b9841dff85bacf540d1bdda4d1a863
youtkddnjs <youtkddnjs91@gmail.com>
2023-12-28 18:36:56 +0900

    Add firestore

---
+++ app/src/main/java/sw/sample/futuremyhome/data/ArticleModel.kt
@@ -0,0 +1,8 @@
+package sw.sample.futuremyhome.data
+
+data class ArticleModel(
+    val articleId: String?= null,
+    val createAt: Long?= null,
+    val description: String?= null,
+    val imageUrl: String?= null,
+)
