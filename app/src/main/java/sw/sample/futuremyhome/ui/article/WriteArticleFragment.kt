a42f54047e14a3317634abeb9f01ae9b80d22784
youtkddnjs <youtkddnjs91@gmail.com>
2024-01-09 19:04:27 +0900

    Add pickMedia

--- app/src/main/java/sw/sample/futuremyhome/ui/article/WriteArticleFragment.kt
+++ app/src/main/java/sw/sample/futuremyhome/ui/article/WriteArticleFragment.kt
@@ -1,7 +1,10 @@
 package sw.sample.futuremyhome.ui.article
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import sw.sample.futuremyhome.R
 import sw.sample.futuremyhome.databinding.FragmentWriteBinding
@@ -9,9 +12,19 @@ import sw.sample.futuremyhome.databinding.FragmentWriteBinding
 class WriteArticleFragment : Fragment(R.layout.fragment_write){
     private lateinit var binding: FragmentWriteBinding
 
+    val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        // Callback is invoked after the user selects a media item or closes the
+        // photo picker.
+        if (uri != null) {
+            Log.d("PhotoPicker", "Selected URI: $uri")
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentWriteBinding.bind(view)
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
-
 }
